3e597acaf2a5933411e6ac1f0d7b07f4fb12fc62
Simon Thompson <simonthompson@Simons-MacBook-Pro.local>
2020-12-28 11:10:40 +0800

    UI tweaks

---
+++ js/scripts.js
@@ -0,0 +1,115 @@
+var count = 2; // number of form people already on page
+function addPerson() {
+    let personInputDiv = document.createElement("div");
+    personInputDiv.id = "person-" + count + "-div";
+    personInputDiv.classList.add("person-div");
+    document.getElementById("script-added-inputs-div").appendChild(personInputDiv);
+    let nameInput = document.createElement("input");
+    nameInput.id = "name-" + count;
+    nameInput.type = "text";
+    nameInput.placeholder = "Name " + (count + 1);
+    nameInput.classList.add("name");
+    personInputDiv.appendChild(nameInput);
+    let amountInput = document.createElement("input");
+    amountInput.id = "amount-" + count;
+    amountInput.type = "number";
+    amountInput.placeholder = "Amount " + (count + 1);
+    amountInput.classList.add("amount");
+    personInputDiv.appendChild(amountInput);
+    count++;
+}
+
+//find average of amounts
+var amounts = [];
+var avg;
+function getAverage(){
+    for (i=0; i<count; i++) {
+        amounts.push(document.getElementById("amount-" + i).value);
+    }
+    var total = 0;
+    for (i=0; i<amounts.length; i++) {
+    total += parseInt(amounts[i]);
+    }
+    avg = total / amounts.length;
+}
+
+//build an array of person objects with name and values
+var sortedPeopleArray = [];
+function buildSortedPeopleArray() {
+    for (i=0; i<count; i++) {
+        let amountInput = document.getElementById("amount-" + i).value;
+        let person = {
+            name: document.getElementById("name-" + i).value,
+            amountPaid: document.getElementById("amount-" + i).value,
+            isSettled: false,
+            diffFromAvg: Math.abs(avg - amountInput),
+        }
+        sortedPeopleArray.push(person);
+    }
+    sortedPeopleArray.sort((a, b) => (a.amountPaid - b.amountPaid));
+}
+
+//error validation functions - 
+//check that amount inputs are numbers, 
+//calculate hasn't been pressed already, 
+//stop if 2 people have same name (or give them different names)
+//clear button
+
+
+//main cost splitting algorithm
+function calculateSplit() {
+    if (document.getElementById("btn-calculate-split").classList.contains("btn-pressed")) {
+        let result = document.createElement("p");
+        result.classList.add("result");
+        result.innerHTML = `Click "Clear All", then try again.`;
+        document.getElementById("results-area").appendChild(result);
+    } else {
+        let top = sortedPeopleArray.length - 1;
+        for (i=0; i<sortedPeopleArray.length; i++) {
+            if (i >= top) {
+                break;
+            } else {
+                if (sortedPeopleArray[i].diffFromAvg < sortedPeopleArray[top].diffFromAvg) {
+                    let result = document.createElement("p");
+                    result.classList.add("result");
+                    result.innerHTML = `${sortedPeopleArray[i].name} pays ${sortedPeopleArray[top].name} $${sortedPeopleArray[i].diffFromAvg}.`;
+                    document.getElementById("results-area").appendChild(result);
+                    sortedPeopleArray[top].diffFromAvg -= sortedPeopleArray[i].diffFromAvg;
+                    sortedPeopleArray[i].diffFromAvg = 0;
+                    sortedPeopleArray[i].isSettled = true;
+                } else if (sortedPeopleArray[i].diffFromAvg > sortedPeopleArray[top].diffFromAvg) {
+                    let result = document.createElement("p");
+                    result.classList.add("result");
+                    result.innerHTML = `${sortedPeopleArray[i].name} pays ${sortedPeopleArray[top].name} $${sortedPeopleArray[i].diffFromAvg}.`;
+                    document.getElementById("results-area").appendChild(result);
+                    sortedPeopleArray[i].diffFromAvg -= sortedPeopleArray[top].diffFromAvg;
+                    sortedPeopleArray[top].diffFromAvg = 0;
+                    sortedPeopleArray[top].isSettled = true;
+                    top--;
+                    i--;
+                } else if (sortedPeopleArray[i].diffFromAvg === sortedPeopleArray[top].diffFromAvg) {
+                    let result = document.createElement("p");
+                    result.classList.add("result");
+                    result.innerHTML = `${sortedPeopleArray[i].name} pays ${sortedPeopleArray[top].name} $${sortedPeopleArray[i].diffFromAvg}.`;
+                    document.getElementById("results-area").appendChild(result);
+                    sortedPeopleArray[top].diffFromAvg = 0;
+                    sortedPeopleArray[top].isSettled = true;
+                    sortedPeopleArray[i].diffFromAvg = 0;
+                    sortedPeopleArray[i].isSettled = true;
+                    top--;
+                }
+            }
+        }
+    }
+}
+
+//change calculate split button to red when pushed
+//when this button changes, the calculate function can't be called again until the page is refreshed
+function changeCalcButton() {
+    document.getElementById("btn-calculate-split").classList.add("btn-pressed");
+}
+
+//refresh page and clear values
+function clearAll() {
+    location.reload();
+}
